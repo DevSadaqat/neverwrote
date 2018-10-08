74f74ec2edbc14093af362910c3178ca8ab79600
Sadaqat Ali <18820040@students.latrobe.edu.au>
2018-10-08 22:53:11 +1100

    updated-controllers

--- api/src/controllers/notebooks.js
+++ api/src/controllers/notebooks.js
@@ -11,6 +11,46 @@ router.get('/', (req, res) => {
     .catch(err => res.status(500).json({ error: err.message }));
 });
 
+  // Create a new notebook record in the database
+router.post('/', (req, res) => {
+  models.Notebook.create(req.body)
+     .then(notebooks => res.json(notebooks))
+   .catch(err => res.status(422).json({ error: err.message }));
+});
+//Show
+router.get('/:notebookId', (req, res) => {
+  // Return the specified notebook record from the database
+  models.Notebook.findById(req.params.notebookId)
+    .then(notebooks => res.json(notebooks))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// Destroy
+router.delete('/:notebookId', (req, res) => {
+  // Delete the specified notebook record from the database
+  models.Notebook.findById(req.params.notebookId)
+    .then(notebooks => notebooks.destroy())
+    .then(() => res.json({}))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// Update
+router.put('/:notebookId', (req, res) => {
+  // Update the specified post record in the database
+  models.Notebook.findById(req.params.notebookId)
+    .then(notebooks => notebooks.update(req.body))
+    .then(notebooks => res.json(notebooks))
+    .catch(err => res.status(422).json({ error: err.message }));
+});
+
+
 /* *** TODO: Fill in the API endpoints for notebooks *** */
+//Returns a list of all notes for a particular notebook
+router.get('/:notebookId/notes', (req, res) => {
+  models.Note.findAll({where: {notebookId: req.params.notebookId} })
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
 
 module.exports = router;
--- api/src/controllers/notes.js
+++ api/src/controllers/notes.js
@@ -5,5 +5,46 @@ const models = require('../models');
 const router = express.Router();
 
 /* *** TODO: Fill in the API endpoints for notes *** */
+//Returns a list of all notes.
+router.get('/', (req, res) => {
+  models.Note.findAll({ order: [['createdAt', 'DESC']] })
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
 
+//Creates a new note using the posted data. The notebookId
+//attribute shall specify which notebook it belongs to.
+//Returns the new note
+router.post('/', (req, res) => {
+  models.note.create(req.body)
+     .then(notes => res.json(notes))
+   .catch(err => res.status(422).json({ error: err.message }));
+});
+
+//Returns a single note by ID.
+router.get('/:noteId', (req, res) => {
+  // Return the specified notes record from the database
+  models.Note.findById(req.params.noteId)
+    .then(notes => res.json(notes))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// Destroy
+router.delete('/:noteId', (req, res) => {
+  // Delete the specified notes record from the database
+  models.Note.findById(req.params.noteId)
+    .then(notes => notes.destroy())
+    .then(() => res.json({}))
+    .catch(err => res.status(500).json({ error: err.message }));
+});
+
+// Update
+router.put('/:noteId', (req, res) => {
+  // Update the specified post record in the database
+  models.Note.findById(req.params.noteId)
+    .then(notes => notes.update(req.body))
+    .then(notes => res.json(notes))
+    .catch(err => res.status(422).json({ error: err.message }));
+});
 module.exports = router;
+
